e9b789d49743019a73b3af809f9c8e1cfb440438
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-04 19:20:43 +0400

    hello user!

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿// Hello, user! Создаем программу, котрая приветствует пользователя.
+Console.WriteLine("Введите ваше имя");
+string userName = Console.ReadLine();
+Console.WriteLine($"Приветствую, {userName}!");
