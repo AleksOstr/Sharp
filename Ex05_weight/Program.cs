3bd3c1f1c0a9713b92bc866886c958a8d2a49c4e
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-04 20:50:13 +0400

    задача с гирями

--- Ex05_weight/Program.cs
+++ Ex05_weight/Program.cs
@@ -6,3 +6,25 @@ int thirdWeight = new Random().Next(1, 101);
 int fourthWeight = new Random().Next(1, 101);
 int fifthWeight = new Random().Next(1, 101);
 Console.WriteLine($"{firstWeight}, {secondWeight}, {thirdWeight}, {fourthWeight}, {fifthWeight}");
+int maxWeight = firstWeight;
+if(firstWeight > maxWeight) 
+{
+    maxWeight = firstWeight;
+}
+if(secondWeight > maxWeight) 
+{
+    maxWeight = secondWeight;
+}
+if(thirdWeight > maxWeight) 
+{
+    maxWeight = thirdWeight;
+}
+if(fourthWeight > maxWeight) 
+{
+    maxWeight = fourthWeight;
+}
+if(fifthWeight > maxWeight) 
+{
+    maxWeight = fifthWeight;
+}
+Console.WriteLine(maxWeight);
