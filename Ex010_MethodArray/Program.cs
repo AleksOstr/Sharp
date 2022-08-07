8277689e731af0bbccf1a9e1a91d004287c287dc
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-07 16:00:17 +0400

    Имеется одномерный массив из n элементов. Требуется найти элемент массива, равный find.

---
+++ Ex010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Имеется одномерный массив из n элементов. Требуется найти элемент массива, равный find.
+
+int[] array = { 14, 21, 31, 41, 15, 61, 37, 28 };
+
+int n = array.Length;
+
+Console.WriteLine("Введите значение искомого элемента");
+int find = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Индекс искомого элемента равен {index}");
+        break;
+    }
+    index++;
+}
