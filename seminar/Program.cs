819c587e476e217bf4fd08c16c779809a1970039
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-07 13:08:49 +0400

    программа, выдающая квадрат числа

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(N*N);
