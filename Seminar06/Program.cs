f61746558f1cf9de1419f78f89857fb1c97efb59
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-09 21:49:11 +0400

    случайное число, программа выдает максимальную цифру этого числа

--- Seminar06/Program.cs
+++ Seminar06/Program.cs
@@ -1,1 +1,15 @@
 ﻿// 
+Console.Clear();
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int num2 = num / 10;
+int num3 = num % 10;
+
+if (num2 > num3)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine(num3);
+}
