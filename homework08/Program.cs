8cb0be253ac66479987abaa4fb2d4fbb5912e11c
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-10 20:10:50 +0400

    Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.

---
+++ homework08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+string number = num.ToString();
+
+if (number.Length != 5)
+{
+    Console.WriteLine("Это не пятизначное число");
+}
+else if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine($"Число {num} - палиндром");
+}
+else
+{
+    Console.WriteLine($"Число {num} - не палиндром");
+}
