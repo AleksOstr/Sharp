024494d7ce4a659df1993d3b9c575ecf5b841085
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-10 20:12:10 +0400

    Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

---
+++ homework010/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= n)
+{
+    Console.Write($"{Convert.ToInt32(Math.Pow(index, 3))} ");
+    index++;
+}
