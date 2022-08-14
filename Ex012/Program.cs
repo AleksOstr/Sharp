c28f4f8d6f47eb742ea899a63445c3e615ae0239
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-14 13:25:11 +0400

    Methods types

---
+++ Ex012/Program.cs
@@ -0,0 +1,55 @@
+﻿// Виды методов
+
+// Вид 1 - Ничего не принимают, ничего не возвращают
+
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();      // Вызов метода
+
+
+// Вид 2 - Принимают аргументы на вход, ничего не возвращают
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения"); // Вызов метода
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Пробное сообщение", count: 3);   // Вызов метода
+
+// Вид 3 - Ничего не принимают, что-то возвращают
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Видд 4 - Что-то принимают, что-то возвращают
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z ");
+Console.WriteLine(res);
