b84e367111752271cf5c8e2cc1df0b5308a7338d
AlexOstryanko <ostryankoam@yandex.ru>
2022-08-31 22:58:36 +0400

    summ of integers from M to N

---
+++ homework028/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+var rand = new Random();
+int m = rand.Next(1, 101);
+int n = rand.Next(1, 101);
+Console.WriteLine($"M = {m} N = {n}");
+int sumOfIntegers = 0;
+if (m > n)
+{
+    for (int i = n; i <= m; i++)
+    {
+        sumOfIntegers += i;
+    }
+    Console.Write($"Сумма натуральных элементов равна {sumOfIntegers}");
+}
+else if (n > m)
+{
+    for (int i = m; i <= n; i++)
+    {
+        sumOfIntegers += i;
+    }
+    Console.Write($"Сумма натуральных элементов равна {sumOfIntegers}");
+}
+else if (m == n)
+{
+    Console.WriteLine("Числа равны");
+}
