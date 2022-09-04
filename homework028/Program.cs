676c5d29f47389f1a633160770e4373c75eb7bb6
AlexOstryanko <ostryankoam@yandex.ru>
2022-09-04 14:56:12 +0400

    final

--- homework028/Program.cs
+++ homework028/Program.cs
@@ -7,4 +7,7 @@ int SumIntegersFromMtoN(int m, int n)
     else return n + SumIntegersFromMtoN(m, n - 1);
 }
 
-Console.WriteLine(SumIntegersFromMtoN(1,5));
+int a = new Random().Next(1, 11);
+int b = new Random().Next(15, 21);
+Console.WriteLine ($"M = {a} N = {b}");
+Console.WriteLine(SumIntegersFromMtoN(a,b));
