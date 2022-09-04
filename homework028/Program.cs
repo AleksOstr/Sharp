ee2bba0d9f13a4669fdd8f996f9772916b6d28dd
AlexOstryanko <ostryankoam@yandex.ru>
2022-09-04 14:52:38 +0400

    Sum integers from M to N recursion

--- homework028/Program.cs
+++ homework028/Program.cs
@@ -1,28 +1,10 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 Console.Clear();
-var rand = new Random();
-int m = rand.Next(1, 101);
-int n = rand.Next(1, 101);
-Console.WriteLine($"M = {m} N = {n}");
-int sumOfIntegers = 0;
-if (m > n)
+int SumIntegersFromMtoN(int m, int n)
 {
-    for (int i = n; i <= m; i++)
-    {
-        sumOfIntegers += i;
-    }
-    Console.Write($"Сумма натуральных элементов равна {sumOfIntegers}");
+    if (n < m) return 0;
+    else return n + SumIntegersFromMtoN(m, n - 1);
 }
-else if (n > m)
-{
-    for (int i = m; i <= n; i++)
-    {
-        sumOfIntegers += i;
-    }
-    Console.Write($"Сумма натуральных элементов равна {sumOfIntegers}");
-}
-else if (m == n)
-{
-    Console.WriteLine("Числа равны");
-}
+
+Console.WriteLine(SumIntegersFromMtoN(1,5));
